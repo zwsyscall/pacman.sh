90e0d4f6c1ea3d8b1dc2d1d3fd57ae2372a2d99f
zwsyscall <61552486+zwsyscall@users.noreply.github.com>
2026-07-09 09:57:14 +0300

    add rss

--- generate.js
+++ generate.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
+const SITE_URL = 'https://pacman.sh';
+const SITE_TITLE = 'pamac';
+const SITE_DESCRIPTION = 'Screaming into the void.';
+
 const docsDir = path.join(__dirname, 'docs');
 
 function getAllMdFiles(dir, fileList = []) {
@@ -49,5 +53,35 @@ const metadata = files.map(filePath => {
 });
 
 metadata.sort((a, b) => new Date(b.date) - new Date(a.date));
+
 fs.writeFileSync('metadata.json', JSON.stringify(metadata, null, 2));
-console.log('metadata.json generated successfully');
+console.log('etadata.json generated successfully');
+
+const rssItems = metadata.map(post => {
+    const pubDate = new Date(post.date).toUTCString();
+
+    const postUrl = `${SITE_URL}/#${post.id}`;
+
+    return `
+    <item>
+      <title><![CDATA[${post.title}]]></title>
+      <link>${postUrl}</link>
+      <guid isPermaLink="true">${postUrl}</guid>
+      <pubDate>${pubDate}</pubDate>
+      <description><![CDATA[New post filed under /${post.folder}]]></description>
+    </item>`;
+}).join('');
+
+const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title><![CDATA[${SITE_TITLE}]]></title>
+    <link>${SITE_URL}</link>
+    <description><![CDATA[${SITE_DESCRIPTION}]]></description>
+    <atom:link href="${SITE_URL}/feed.xml" rel="self" type="application/rss+xml" />
+${rssItems}
+  </channel>
+</rss>`;
+
+fs.writeFileSync('feed.xml', rssFeed.trim());
+console.log('feed.xml generated successfully');
